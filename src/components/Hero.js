7f7eb50a4c1b979794fcf6a971593040206ad6e7
harsh-chandak <harshnchandak@gmail.com>
2025-05-25 16:09:21 -0700

    Routing changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -78,7 +78,7 @@ export default function Hero() {
                     A full-stack developer | MSCS @ASU'26
                 </p>
                 <a
-                    href="#timeline"
+                    href="#about"
                     className="text-3xl inline-block px-6 py-2 border border-red-400 text-red-400 rounded hover:bg-red-400 hover:text-white transition margin-10"
                 >
                     More About Me ↓
